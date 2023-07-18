5252eff87148e0be8607c5d607fe91fe63749094
JC Nicolas <jeancnicolas@icloud.com>
2023-07-18 22:13:06 +0100

    sleepy

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,9 +1,10 @@
 import { getCollection } from "astro:content";
-import { LayoutProps } from '../components/types'
+import type { LayoutProps } from '../components/types';
+
 interface Data {
   data: LayoutProps
 }
-export const getProps = async (target: string) => {
+export async function getProps(target: string) {
   const projectList = await getCollection("projects");
   return projectList.find(({ slug }) => slug === target) as Data
 }
