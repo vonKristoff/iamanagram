97f343cc4e4295f334c32ce474e0aae4b8d38184
JC Nicolas <jeancnicolas@icloud.com>
2023-08-26 16:17:23 +0100

    update bg colours

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -4,10 +4,16 @@ import type { LayoutProps } from '../components/types';
 type Data = {
   data: LayoutProps | null
 }
+
+/**
+ * notes from discord
+ * If you are within [pagename].astro, and you are already iterating through the result of getCollection() within getStaticPaths(), you can also pass the actual entry directly via the props.
+ */
+
 export async function getProps(target: string): Promise<any> {
   const [pagename] = target.split('/').pop()?.split('.') || ''
   const projectList = await getCollection("projects");
   const result = projectList.find(({ slug }) => slug === pagename)
-  console.log(pagename, result)
+  // console.log(pagename, result)
   return result ? result.data : {}
 }
